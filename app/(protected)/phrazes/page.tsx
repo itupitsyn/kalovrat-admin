dc01973727c369056997ae14f2111ca5b6d444e5
itupitsyn <itupitsyn@ptsecurity.com>
2025-11-26 03:18:42 +0300

    feat(phrazes): pagination

--- app/(protected)/phrazes/page.tsx
+++ app/(protected)/phrazes/page.tsx
@@ -2,13 +2,31 @@ import { AddPhrazeForm } from '@/components/app/add-phraze-form';
 import { PhrazesRow } from '@/components/app/phrazes-row';
 import { TableWrapper } from '@/components/app/table-wrapper';
 import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { PAGE_SIZE } from '@/lib/constants';
 import prisma from '@/lib/prisma';
+import { PageParams } from '@/lib/types';
+import { getPageNumber } from '@/lib/utils';
 
-export default async function Page() {
-  const data = await prisma.phrazes.findMany();
+export default async function Page(params: PageParams) {
+  const page = await getPageNumber(params);
+
+  const [data, total] = await Promise.all([
+    prisma.phrazes.findMany({
+      skip: (page - 1) * PAGE_SIZE,
+      take: PAGE_SIZE,
+    }),
+    prisma.phrazes.aggregate({ _count: true }),
+  ]);
 
   return (
-    <TableWrapper title="Фразы">
+    <TableWrapper
+      title="Фразы"
+      pagination={{
+        generateLink: (pageNumber) => `/phrazes?page=${pageNumber}`,
+        page,
+        totalItems: total._count,
+      }}
+    >
       <AddPhrazeForm />
 
       <Table className="mt-4">
